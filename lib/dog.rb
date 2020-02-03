73fcd8d4d8e7bd841695fbcd2dae73e8081c7aa8
Daniel Sun <danielsun12345@gmail.com>
2020-02-03 23:48:49 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def name=(this_dog)
+    @names = this_dog
+  end 
+  
+  def name
+    @names
+  end 
+  
+  def breed=(this_breed)
+    @breed = this_breed
+  end
+  
+  def breed
+    @breed
+  end 
+    
+end 
